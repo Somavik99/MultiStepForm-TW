37c500f26eeeabf1c41538069f114af8243374f4
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-27 20:25:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Form from "./Components/Forms/Form";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Form</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Forms/Form.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='Form'>
+        <div className="Form_container">
+            <div className="header">
+
+            </div>
+            <div className="body">
+
+            </div>
+            <div className="footer">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
