94e3d105baa5495bef5452e9343b5fbc08987848
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-30 09:58:40 +0530

    Context created

---
+++ src/Components/Context/Context.jsx
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+export const FormContext = createContext();
+const Context = () => {
+    
+  return <div>Context</div>;
+};
+
+export default Context;
--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  ArrowUturnLeftIcon,
+  ArrowRightIcon,
+} from "@heroicons/react/24/outline";
 
 const Form = () => {
+  const [FormStep, setFormStep] = useState(0);
+  const FormHeader = ["Sign Up", "Personal Info", "Location", "Plans"];
   return (
-    <div className='Form'>
-        <div className="Form_container">
-            <div className="header">
-
-            </div>
-            <div className="body">
-
-            </div>
-            <div className="footer">
-                
-            </div>
+    <div className="Form">
+      <div className="Form_container">
+        <div className="header">
+          <h1>{FormHeader[FormStep]}</h1>
         </div>
+        <div className="body"></div>
+        <div className="footer">
+          <button>
+            <ArrowUturnLeftIcon className="w-1 h-1" />
+            Back
+          </button>
+          <button>
+            <ArrowRightIcon className="w-1 h-1" />
+            Next
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
